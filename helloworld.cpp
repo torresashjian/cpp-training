c57050ec6ec82f3d80c472c7b7097aac01f11f4a
Miguel Torres <mtorres@tibco.com>
2015-11-26 13:26:23 -0800

    Add puts and comments

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <string.h>
+#include <stdio.h>
 
+/**
+* Main method on hello world
+**/
 int main(){
+	// Adding some comments here
 	std::cout << "Hello World" << std::endl;
+	int y;
+	y = 1 + 1;
+	// Print y
+	std::cout << y << std::endl;
+	char putStr []  = "Test Puts";
+	puts(putStr);
 	return 0;
-}
+} 
