8bcfc6b38a3d34f185d8248fba923b68451c307a
Miguel Torres <mtorres@tibco.com>
2015-11-27 13:39:31 -0800

    Added cin

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,17 +2,34 @@
 #include <string.h>
 #include <stdio.h>
 
+/** Function Declaration **/
+void testcout();
+void testcin();
+
 /**
 * Main method on hello world
 **/
 int main(){
 	// Adding some comments here
+	//testcout();
+	testcin();
+	return 0;
+} 
+
+void testcout(){
 	std::cout << "Hello World" << std::endl;
 	int y;
 	y = 1 + 1;
 	// Print y
-	std::cout << y << std::endl;
+	std::cout << "Value of y: "<< y << std::endl;
 	char putStr []  = "Test Puts";
 	puts(putStr);
-	return 0;
-} 
+}
+
+void testcin(){
+	std::cout << "Enter a number: " << std::endl;
+	int x = 0;
+	std::cin >> x;
+	std::cout << "Number Entered: " << x << std::endl;
+}
+	
